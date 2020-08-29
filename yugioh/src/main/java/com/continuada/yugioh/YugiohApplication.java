0800f46ed9ac91a4b1a1e3aee650c5d1def0cda5
italo-andrade-99 <italo.andrade@bandtec.com.br>
2020-08-29 16:40:19 -0300

    Subindo projeto

---
+++ yugioh/src/main/java/com/continuada/yugioh/YugiohApplication.java
@@ -0,0 +1,13 @@
+package com.continuada.yugioh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugiohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YugiohApplication.class, args);
+	}
+
+}
